87c38c5b2208c34e9e41ab8ba4f611a3718ad006
IlyasDevelopment <85805215+IlyasDevelopment@users.noreply.github.com>
2021-06-15 13:31:09 +0300

    Update main_fast.py

--- Homework_2/E_cows/main_fast.py
+++ Homework_2/E_cows/main_fast.py
@@ -2,7 +2,7 @@ num = int(input())
 seq = list(map(int, input().split()))
 vasya_values = []
 winner = max(seq)
-for i in range(seq.index(max(seq)) + 1, num - 1):
+for i in range(seq.index(winner) + 1, num - 1):
     if (seq[i] - 5) % 10 == 0 and seq[i] > seq[i + 1]:
             vasya_values.append(seq[i])
 if not vasya_values:
