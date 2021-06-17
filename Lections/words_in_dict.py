e61bbe77f192a04d29567b232bca30ce6471391b
IlyasDevelopment <85805215+IlyasDevelopment@users.noreply.github.com>
2021-06-17 11:49:41 +0300

    Create words_in_dict.py

---
+++ Lections/words_in_dict.py
@@ -0,0 +1,19 @@
+def wordsindict(dictionary, text):
+    goodwords = set(dictionary)
+    for word in dictionary:
+        for delpos in range(len(word)):
+            goodwords.add(word[:delpos] + word[delpos+1:])
+    print('goodwords:', goodwords)
+    ans = []
+    for word in text:
+        ans.append(word in goodwords)
+    return ans
+
+
+dict = {}
+dict['abcd'] = 4
+dict['eft'] = 2
+print('set(dict) -> initial goodwords:', set(dict))
+for word in dict:
+    print(word)
+print(wordsindict(dict, ['abcd', 'its', 'me']))
