de09e2b919c9cadfce24d30812f1eef76e23a05c
IlyasDevelopment <85805215+IlyasDevelopment@users.noreply.github.com>
2021-07-23 21:30:45 +0300

    Create main.py

---
+++ Homework_8/A_tree_height/main.py
@@ -0,0 +1,48 @@
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert(value, self.root)
+
+    def _insert(self, value, cur_node):
+        if value < cur_node.value:
+            if cur_node.left is None:
+                cur_node.left = Node(value)
+            else:
+                self._insert(value, cur_node.left)
+        elif value > cur_node.value:
+            if cur_node.right is None:
+                cur_node.right = Node(value)
+            else:
+                self._insert(value, cur_node.right)
+
+    def height(self):
+        if self.root is not None:
+            return self._height(self.root, 0)
+        else:
+            return 0
+
+    def _height(self, cur_node, cur_height):
+        if cur_node is None:
+            return cur_height
+        left_height = self._height(cur_node.left, cur_height + 1)
+        right_height = self._height(cur_node.right, cur_height + 1)
+        return max(left_height, right_height)
+
+
+values = list(map(int, input().split()))[:-1]
+tree = BinarySearchTree()
+for value in values:
+    tree.insert(value)
+print(tree.height())
